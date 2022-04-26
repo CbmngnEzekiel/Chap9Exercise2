601185c9dd00fa32b6bf47b9b5bd7546013a77fd
Ezekiel Cabamongan <cabamonganezekiel@gmail.com>
2022-04-26 11:41:07 +0800

    cleaning

--- src/App.java
+++ src/App.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
 public class App {
-    
+    static Scanner sc = new Scanner(System.in);
     
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
 
         System.out.println("Open an Account: \n" +
                             "[1] Personal Account\n" +
@@ -12,28 +11,7 @@ public class App {
         int choice = sc.nextInt();
         sc.nextLine();
 
-        System.out.print("Enter first name: ");
-        String firstName = sc.nextLine();
-
-        System.out.print("\nEnter last name: ");
-        String lastName = sc.nextLine();
-
-        System.out.print("\nEnter street: ");
-        String street = sc.nextLine();
-
-        System.out.print("\nEnter city: ");
-        String city = sc.nextLine();
-
-        System.out.print("\nEnter state: ");
-        String state = sc.nextLine();
-
-        System.out.print("\nEnter zip: ");
-        String zip = sc.nextLine();
-
-        System.out.print("\nEnter Amount: ");
-        double amount = sc.nextDouble();
-
-        Account account = createAccount(choice, amount, firstName, lastName, street, city, state, zip);
+        Account account = createAccount(choice);
 
         int menuChoice = 0;
 
@@ -72,11 +50,32 @@ public class App {
         sc.close();
     }
 
-    static public Account createAccount(int choice, double balance, String firstName, String lastName, String street, String city, String state, String zip){
+    static public Account createAccount(int choice){
+        System.out.print("Enter first name: ");
+        String firstName = sc.nextLine();
+
+        System.out.print("\nEnter last name: ");
+        String lastName = sc.nextLine();
+
+        System.out.print("\nEnter street: ");
+        String street = sc.nextLine();
+
+        System.out.print("\nEnter city: ");
+        String city = sc.nextLine();
+
+        System.out.print("\nEnter state: ");
+        String state = sc.nextLine();
+
+        System.out.print("\nEnter zip: ");
+        String zip = sc.nextLine();
+
+        System.out.print("\nEnter Amount: ");
+        double amount = sc.nextDouble();
+        
         if(choice == 1){
-            return new PersonalAcct(balance, firstName, lastName, street, city, state, zip);
+            return new PersonalAcct(amount, firstName, lastName, street, city, state, zip);
         }else{
-            return new BusinessAcct(balance, firstName, lastName, street, city, state, zip);
+            return new BusinessAcct(amount, firstName, lastName, street, city, state, zip);
         }
     }
 }
